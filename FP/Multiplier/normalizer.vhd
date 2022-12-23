31c36f051b823ae800938a6c0a9b5e3434d0f625
MinaAbdelmaseeh <118192329+MinaAbdelmaseeh@users.noreply.github.com>
2022-12-23 17:20:18 +0200

    Update normalizer.vhd

--- FP/Multiplier/normalizer.vhd
+++ FP/Multiplier/normalizer.vhd
@@ -1,36 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 12/23/2022 12:14:14 PM
--- Design Name: 
--- Module Name: normalizer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity normalizer is
     generic (NBITS   : natural :=8;
