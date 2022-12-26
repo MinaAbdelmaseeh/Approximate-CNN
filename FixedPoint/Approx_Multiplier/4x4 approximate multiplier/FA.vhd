2f9b797deb534c611152cb8941c8e6999cf665b6
khaledkhallaf <51542454+khaledkhallaf@users.noreply.github.com>
2022-12-26 02:23:31 +0200

    Add files via upload

---
+++ FixedPoint/Approx_Multiplier/4x4 approximate multiplier/FA.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/24/2022 11:10:38 PM
+-- Design Name: 
+-- Module Name: FA - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FA is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Cin : in STD_LOGIC;
+           Cout : out STD_LOGIC;
+           S : out STD_LOGIC);
+end FA;
+
+architecture Behavioral of FA is
+begin
+S<= A xor B xor Cin;
+Cout <= (A and B) or (B and Cin) or (A and Cin);
+
+end Behavioral;
